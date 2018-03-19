53da0e2131cb8eb3ffa5e2ead2cb9d32a104e2ae
lululuka <dnakv321@gmail.com>
2018-03-20 01:15:42 +0800

    my first vue-cli-project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <todos :todos="todos"/>
+    <todo-form :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
+
+export default {
+  name: 'App',
+    data(){
+        return {
+          todos:[
+                  {id:1,title:'Learn Vuejs', completed:false}
+           ]
+        }
+    },
+    computed: {
+       todoCount() {
+         return this.todos.length;
+       }
+     },
+   components: {
+     HelloWorld,Todos,TodoForm
+   }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form v-on:submit.prevent="addTodo(newTodo)">
+      <div class="from-group">
+          <input type="text"
+                 v-model="newTodo.title"
+                 class="form-control"
+                 placeholder="Add a new todo">
+      </div>
+      <br>
+      <div class="form-group">
+          <button class="btn btn-success" type="submit">Add Todos</button>
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+      props:['todos'],
+      data(){
+        return {
+            newTodo:{id:null,title:'', completed:false}
+        }
+      },
+      methods:{
+          addTodo(newTodo){
+              this.todos.push(newTodo),
+              this.newTodo = {id:null,title:''}
+          }
+      }
+}
+</script>
+
+<style>
+  .completed {
+    color: #5cb85c;
+    text-decoration: line-through;
+  }
+  .margin-right-10 {
+    margin-right: 10px;
+  }
+
+</style>
